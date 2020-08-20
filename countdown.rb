2bc472691c2daf11f0b1dff928dcaebc62d4cfae
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 17:35:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,5 +8,12 @@ def countdown (i)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    sleep(1)
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
   
